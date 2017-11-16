03c423d4cc4cfa4312e9fceac0faa55981cbb34c
Albert Costas <albert.costas@privalia.com>
2017-11-16 12:24:49 +0100

    Add collback getnames

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,10 +3,24 @@ import SideBar from './SideBar';
 import MainContent from './MainContent';
 
 const Content = (props) => {
+
+    function getNames(sections) {
+        return sections.map(sections => { //no hemos puesto parentesis porque solo es un parametro (sections) =>
+                return (<li key={sections.id} className='collection-item'
+                            onClick={(e) => onSectionSelected(sections.id, e)}>{sections.name}</li>)
+            }
+        );
+    }
+
+    function onSectionSelected(id, e) {
+        e.preventDefault();
+        console.log('This link was clicked with ' + id)
+    }
+
     return (
         <div className='container'>
             <div className='row'>
-                <SideBar sections={props.sections}/>
+                <SideBar getNames={getNames(props.sections)}/>
                 <MainContent section={props.sections[0].content}/>
             </div>
         </div>);
